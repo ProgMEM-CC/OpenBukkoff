12ba75dd2b0f92da76553184d28f0dcf11f9af08
6x68 <249649284+fun-fact-is-that-everything-after-the-plus-sign-is-useless@users.noreply.github.com>
2026-02-05 12:30:50 -0600

    bottom text

--- src/main/kotlin/cc/progmem/bukkoff/Main.kt
+++ src/main/kotlin/cc/progmem/bukkoff/Main.kt
@@ -1,11 +1,19 @@
 package cc.progmem.bukkoff
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.jthemedetecor.OsThemeDetector
 
 fun main() = application {
     Window(
@@ -16,8 +24,22 @@ fun main() = application {
     }
 }
 
+val darkScheme = darkColorScheme()
+
 @Composable
 @Preview
 fun App() {
-    Text("I don't like niggers")
+    MaterialTheme(colorScheme = darkScheme) {
+        Column(
+            modifier = Modifier
+                .background(darkScheme.primaryContainer)
+                .safeContentPadding()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Bottom,
+        ) {
+            // why do I have to specify the color so it isn't black.
+            Text("bottom text", color = darkScheme.primary)
+        }
+    }
 }
